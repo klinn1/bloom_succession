aa1fe2c708715ded877a72bc37175a44216ed3d2
Kyla Linn <klinn1@vols.utk.edu>
2021-08-30 12:51:42 -0400

    updated 8/30 after meeting

--- model_with_nutrients.py
+++ model_with_nutrients.py
@@ -3,26 +3,27 @@ from matplotlib.lines import Line2D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import math
+import sys
 
 #parameter values
-phi1 = 0.4 #interaction strength for p1
+phi1 = 0.2 #interaction strength for p1
 phi2 = 0.1 #interaction strength for p2
 eps1 = 0.4 #transfer efficiency for p1
 eps2 = 0.3 #transfer efficiency for p2
-delta_c = 0.001 #consumer mortality
+delta_c = 0.1 #consumer mortality
 k_w = 0.02 #light attenuation by just water
 k_p = 0.001 #attenuation due to producers
-delta1 = 0.001 #death rate of producer 1
-delta2 = 0.001 #death rate of producer 2
-mu1 = 2.0 #resource affinity parameter for p1
-mu2 = 1.0 #resource affinity parameter for p2
+delta1 = 0.1 #death rate of producer 1
+delta2 = 0.1 #death rate of producer 2
+mu1 = 3.0 #resource affinity parameter for p1
+mu2 = 2.0 #resource affinity parameter for p2
 alpha_n = 500 #saturation of nutrients
 alpha_i = 1.0 #saturation of light
-S_R = 0.2 #supply rate
+S_R = 0.4 #supply rate
 
 # time change (discrete time)
 T = 365.0 #days
-dt = 0.5
+dt = 0.04
 #time array
 t = np.linspace(0, T, int(T/dt)) 
 
@@ -34,37 +35,16 @@ N = np.zeros((len(t),len(zetas)))
 P1 = np.zeros((len(t),len(zetas)))
 P2 = np.zeros((len(t),len(zetas)))
 C = np.zeros((len(t),len(zetas)))
-N[0] = 0.5
-P1[0] = 0.5
-P2[0] = 0.5
-C[0] = 0.5
+N[0,:] = 0.4
+P1[0,:] = 0.5
+P2[0,:] = 0.5
+C[0,:] = 0.5
 #print(N.shape)
-
-
-#function for nutrients
-def nutrients(N):
-    dNdt = S_R - (mu1*(N/(N+alpha_n))*P1[i-1]) - (mu2*(N/(N+alpha_n))*P2[i-1])
-    return dNdt
 
 #light function
 def irradiance_function(zetas, P1,P2, k, l, dep_size):
     I = I_naughts[k] * np.exp(-(k_w*zetas[l]+np.sum(k_p*(P1[:l]+P2[:l])*dep_size)))
     return I
-
-#function for P1
-def producer1(P1):
-    dP1dt = (mu1*(N[i-1]/(N[i-1]+alpha_n))*P1) - (phi1*P1*C[i-1]) - (delta1*P1)     
-    return dP1dt
-
-#function for P2
-def producer2(P2):
-    dP2dt = (mu2*(N[i-1]/(N[i-1]+alpha_n))*P2) - (phi2*P2*C[i-1]) - (delta2*P2) 
-    return  dP2dt
-
-#function for C
-def consumer(C):
-    dCdt = (eps1*phi1*P1[i-1]*C) + (eps2*phi2*P2[i-1]*C) - (delta_c*C)        
-    return dCdt
 
 #light array
 l = 0
@@ -90,11 +70,15 @@ print('irradiance array from contour figure', lights.shape)
  
 #building 2 dimensional arrays for nutrients, producer, and consumer groups
 for i in  range(1, len(t)):
-    for i in range(1,len(zetas)):
-        N[i] = N[i-1] + nutrients(N[i -1])* dt
-        P1[i] = P1[i-1] + producer1(P1[i -1])* dt
-        P2[i] = P2[i-1] + producer2(P2[i -1])* dt
-        C[i] = C[i-1] + consumer(C[i -1])* dt
+    for j in range(0,len(zetas)):
+        dNdt = S_R - (mu1*N[i-1,j]*P1[i-1,j]) - (mu2*N[i-1,j]*P2[i-1,j])
+        dP1dt = (mu1*N[i-1,j]*P1[i-1,j]) - (phi1*P1[i-1,j]*C[i-1,j]) - (delta1*P1[i-1,j])
+        dP2dt = (mu2*N[i-1,j]*P2[i-1,j]) - (phi2*P2[i-1,j]*C[i-1,j]) - (delta2*P2[i-1,j])
+        dCdt = (eps1*phi1*P1[i-1,j]*C[i-1,j]) + (eps2*phi2*P2[i-1,j]*C[i-1,j]) - (delta_c*C[i-1,j])
+        N[i] = N[i-1,j-1] + dNdt * dt
+        P1[i] = P1[i-1,j-1] + dP1dt * dt
+        P2[i] = P2[i-1,j-1] + dP2dt * dt
+        C[i] = C[i-1,j-1] + dCdt * dt
 
 # print shape of arrays for nutrients, producer, and consumer groups
 print('N shape = ', N.shape)
